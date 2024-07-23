2c3de2fd07039daa5f9f9f5232c99c62a8811d72
Salil Thakur <salilthakur09@gmail.com>
2024-07-24 03:58:53 +0530

    recursion2

---
+++ Recursion.cpp
@@ -0,0 +1,30 @@
+// print name as number of times we want using recursion.
+/*#include<iostream>
+using namespace std;
+void pname(int i, int n){
+    if(i>n)return;
+    cout<<"salil"<<endl;
+    pname(i+1, n);
+}
+int main(){
+    int i=1,n;
+    cin>>n;
+    pname(i, n);
+    return 0;
+}*/
+
+
+// print linearly from 1 to n.
+#include<iostream>
+using namespace std;
+void pnum(int i){
+    if(i<1)return;
+    cout<<i<<endl;
+    pnum(i-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    pnum(n);
+    return 0;
+}
