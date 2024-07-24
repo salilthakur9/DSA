353fd6064cf79b5a37925fdbdad6058f64b46e25
Salil Thakur <salilthakur09@gmail.com>
2024-07-25 00:38:19 +0530

    25-7

--- Recursion.cpp
+++ Recursion.cpp
@@ -74,3 +74,21 @@ int main(){
     int n;cin>>n;
     pnum(n);return 0;
 }*/
+
+
+
+#include<iostream>
+using namespace std;
+void func(int i,int sum){
+    if(i<1){
+        cout<<sum;
+        return;
+    }
+    func(i-1,sum+i);
+}
+int main(){
+    int i;cin>>i;
+    func(i,0);
+    return 0;
+
+}
