9239af34ef77b7d667c7bfc7ede636e8c3019d04
Salil Thakur <salilthakur09@gmail.com>
2024-07-26 00:41:51 +0530

    26-7

--- Recursion.cpp
+++ Recursion.cpp
@@ -107,4 +107,18 @@ int main(){
 }*/
 
 
+//functional recursion factorial of input number.
+/*#include<iostream>
+using namespace std;
+int func(int n){
+    if(n==1)return 1;
+    return n*func(n-1);
+}
+int main(){
+    int n;cin>>n;
+    cout<<func(n);
+    return 0;
+}*/
+
+
 
