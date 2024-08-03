07053b84e5671ee734cdf81ac741e2b25ddc6560
Salil Thakur <salilthakur09@gmail.com>
2024-08-04 04:23:12 +0530

    4-7

--- Hash.cpp
+++ Hash.cpp
@@ -59,3 +59,63 @@ int main(){
     }
     return 0;
 }*/
+
+
+// 3. by using maps hash array to check the occurence of the element or number
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    map<int, int>mp;
+    for(int i=0;i<n;i++){
+        mp[arr[i]]++;
+    }
+
+    int q;cin>>q;
+    while(q--){
+        int number;
+        cin>>number;
+        cout<<mp[number]<<endl;
+    }
+    return 0;
+}*/
+
+
+// 4. use iterator in ques 3
+/*#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    map<int, int>m;
+    for(int i=0;i<n;i++){
+        m[arr[i]]++;
+    }
+
+    for(auto it:m){
+        cout<<it.first<<"->"<<it.second<<endl;
+    }
+
+    int q;
+    cin>>q;
+    while(q--){
+        int num;
+        cin>>num;
+        cout<<m[num]<<endl;
+    }
+    return 0;
+}*/
+
+
+
